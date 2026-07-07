f25e54a95440bb77c68b69c90dd865c8942a4c70
YoyoTech1B <yoyoblue30@gmail.com>
2026-07-07 22:48:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,21 @@
   HEADER SCROLL EFFECT
 ==================================================*/
 
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
 
 
 /*==================================================
